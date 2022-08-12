3a423ea9fc07bad563d5695a9048df7feed1094e
viggo-gascou <94069687+viggo-gascou@users.noreply.github.com>
2022-08-12 15:21:41 +0200

    add yellow, fix question and add emojis

--- home/.chezmoiscripts/run_once_install-anaconda.sh
+++ home/.chezmoiscripts/run_once_install-anaconda.sh
@@ -13,30 +13,31 @@ set -e # -e: exit on error
 
 RED='\033[1;31m' # bold red
 GREEN='\033[32;1m' # bold green
-BOLD='\033[1m' # bold'
-NC='\033[0m' # No Color
+YELLOW='\033[033;1m'
+BOLD='\033[1m' # bold
+NC='\033[0m' # no Color
 
 
 if command -v conda >/dev/null 2>&1; then
-  echo -e "${GREEN}✅ Anaconda already installed, skipping.${NC}"
+  echo "${GREEN}✅ Anaconda already installed, skipping.${NC}"
   else
-    echo -e "\n${BOLD}Please go to https://bit.ly/condainstall and install the latest conda/anaconda version\n"
+    echo "\n${BOLD}Please go to https://bit.ly/condainstall and install the latest conda/anaconda version\n"
     read -r -p "Have you successfully installed anaconda? Type 'yes' to continue with the installation."$'\n' REPLY
-    echo -e "${NC}"
+    echo "${NC}"
   if [[ "$REPLY" =~ ^([yY][eE][sS]|[yY])$ ]] 1>/dev/null; then
-    echo -e "${GREEN}✅ Anaconda successfully installed${NC}"
+    echo "${GREEN}✅ Anaconda successfully installed${NC}"
   else
-    echo -e "${RED}❌ Anaconda is needed to continue the dotfile installation process! Please retry the installation.${NC}"
+    echo "${RED}❌ Anaconda is needed to continue the dotfile installation process! Please retry the installation.${NC}"
     exit 0
   fi
 fi
 
 if command -v conda >/dev/null 2>&1; then # making sure that conda is installed
   echo "${BOLD}"
-  if read -q "choice?Do you want to create a kattis specific conda environment? Press Y/y to continue"$'\n'
-  then
+  read -r -p "Do you want to create a kattis specific conda environment? Type 'yes' to continue."$'\n' REPLY
+  if [[ "$REPLY" =~ ^([yY][eE][sS]|[yY])$ ]] 1>/dev/null; then
     echo 
-    echo "Creating kattis conda environment...${NC}"
+    echo "🛠 Creating kattis conda environment...${NC}"
     conda create -n pykat
     conda activate pykat
     conda config --env --set subdir osx-64
@@ -46,7 +47,8 @@ if command -v conda >/dev/null 2>&1; then # making sure that conda is installed
     echo "${GREEN}✅ Successfully created kattis conda environment (pykat) with Python 3.7.10!${NC}"
   else
     echo
-    echo "${BOLD}kattis conda environment will not be created, skipping.${NC}"
+    echo "${YELLOW}⏩ kattis conda environment will not be created, skipping.${NC}"
     exit 0
+    fi
   fi
 fi
